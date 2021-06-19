7056e021e1caf9f268f608d236af1f129c43a5a3
Krishnakrish05 <47746860+Krishnakrish05@users.noreply.github.com>
2021-06-19 13:58:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import csv
-
+def dictlist():
 with open('project.csv','r') as files:
         reader =csv.DictReader(files)
         for column in reader:
                 print(dict(column))
+                
+dictlist()                
+                
 
 
